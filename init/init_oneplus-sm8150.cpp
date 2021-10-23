b2edd23fc78d7dce2d982dbd758bbbfda9f7248a
jhonboy121 <alfredmathew05@gmail.com>
2021-10-23 17:36:38 +0530

    init: update fingerprint to redfin SP1A.210812.015
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- init/init_oneplus-sm8150.cpp
+++ init/init_oneplus-sm8150.cpp
@@ -201,5 +201,9 @@ void vendor_load_properties() {
   load_dalvikvm_properties();
 
   // fingerprint
-  property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/coral/coral:11/RQ3A.211001.001/7641976:user/release-keys");
+  property_override_multi("ro.build.fingerprint",
+    "ro.vendor.build.fingerprint",
+    "ro.bootimage.build.fingerprint",
+    "google/redfin/redfin:12/SP1A.210812.015/7679548:user/release-keys"
+  );
 }
